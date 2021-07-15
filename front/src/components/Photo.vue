c2c68593545d263b76c404f00953a5f60d23989f
UltimateHikari <andy@andystpad.localdomain>
2021-07-15 15:21:10 +0700

    fetching info to image info if endpoint is not "random"

--- front/src/components/Photo.vue
+++ front/src/components/Photo.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="lightbox" @click.self="close">
-    <div v-if="this.loadingStatus" class='loading-div'>
+    <div v-if="loadingStatus" class='loading-div'>
       <div class="spinner-border text-info m-5" role="status">
         <span class="sr-only">Loading...</span>
       </div>
@@ -9,29 +9,44 @@
       <img class="rounded" :src="imgsrc">      
     </div>
     <div class="lightbox-info">
-      <div class="lightbox-info-inner">
+      <div v-if="isRandomEffect" class="lightbox-info-inner">
         <p>Magic is <s>friendship</s> two sequentially applied random image transformations and random rotation</p>
         <p>However, EffectService would have elaborated on that in http response if he was able to</p>
+      </div>
+      <div v-else class="lightbox-info-inner">
+        <p v-if="photo.title">{{ photo.title }}</p><p v-if="photo.location">{{ photo.location }}</p><p v-if="photo.photographer">
+          <a rel="nofollow" :href="photo.photographer.url">
+            {{ photo.photographer.name }}
+          </a>
+        </p><p v-if="photo.source">
+          via
+          <a rel="nofollow" :href="photo.source.url">
+            {{ photo.source.name }}
+          </a>
+        </p>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 import apiurl from "@/path.js"
+
+const axios_instance = axios.create({
+  baseURL: apiurl + "list",
+})
 
 export default {
   name: 'Photo',
   props: {
-    query: {
-      type: Number,
-      default: 1000
-    }
+    props: [],
   },
   data() {
     return {
       imgsrc: "",
       loadingStatus: true,
+      isRandomEffect: true,
     };
   },
   computed: {
@@ -42,7 +57,15 @@ export default {
     },
   },
   created() {
-
+    axios_instance.get().then((result) => {
+      this.photos = result.data;
+      if(this.$route.params.id != "random"){
+      this.isRandomEffect = false;
+      }
+    }, error => {
+      console.error(error);
+    });
+    
     var myImage = new Image();
     myImage.src = this.photoUrl(this.$route.params.id);
     myImage.onload = () => {
